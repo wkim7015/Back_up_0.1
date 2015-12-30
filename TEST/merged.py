92d0866e90f245b01ed09115f0fcb1b956d2c868
wkim7015 <u5125048@anu.edu.au>
2015-12-30 16:25:51 +0900

    Create merged.py

---
+++ TEST/merged.py
@@ -0,0 +1,8 @@
+import unittest
+from TESTcopy_function import*
+from TESTaddress_function import *
+from TESTlog_function import *
+#######################
+
+if __name__ == "__main__":
+    unittest.main()

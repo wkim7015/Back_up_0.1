2b1d45038824b41bc6c00374fc71bc79bb45fc37
wkim7015 <u5125048@anu.edu.au>
2016-01-06 14:46:04 +0900

    Update TESTlog_function.py

--- TEST/TESTlog_function.py
+++ TEST/TESTlog_function.py
@@ -6,7 +6,6 @@ import os.path
 sys.path.append(os.path.realpath('..'))
 from log_function import *
 from datetime import *
-
 import string
 import random
 
@@ -20,6 +19,7 @@ input4 = directoryName+"\\testfiletxt"
 input5 = directoryName+"\\tEstFile2.tXT"
 input6 = directoryName+"\\tEstFile3.tXT"
 input2 = directoryName+"\\될까.txt"
+
 
 ###########################======================================
 # text and directory paths to keep logs. Be aware of the directoryPath which needs to be emptied, for simplicity's sake.
@@ -63,9 +63,9 @@ class TESTmake_report_open_report(unittest.TestCase):
     global input6
     global input2
       
-    path =  (input1).decode('cp949').encode('cp949')
+    path =  (input1).decode('utf-8').encode('cp949')
     #path2 =  ("D:\\Users\\Hojin\\Desktop\\testfile.txt").decode('cp949').encode('cp949') #> 디렉토리가 없을시, 변환 불가능. open 에서 에러
-    path3 =  (input3).decode('cp949').encode('cp949') # 만약 \\ 가 안될시? : eclipse 폴더 안에 저장됨
+    path3 =  (input3) # 만약 \\ 가 안될시? : eclipse 폴더 안에 저장됨
     #path4 =  (input4).decode('cp949').encode('cp949') #민약 파일이름이 기호 일시 실험 >기호사용한 이름 파일 지원 불가능
     path5 =  (input4) #만약 파일이름이 한국어 일때 실험 > 가능
     String = (input5) #Result: create the file without extension
@@ -74,7 +74,7 @@ class TESTmake_report_open_report(unittest.TestCase):
     #no way for escape strings
     def test_existence(self):# worked.
         file2 = text_log().make_report(self.path3)
-        file_check1  = os.path.isfile(self.path3)
+        file_check1  = os.path.isfile(unicode_conversion(self.path3))
         self.assertTrue(file_check1,msg = 'True')
 
 
@@ -100,7 +100,7 @@ class TESTmake_report_open_report(unittest.TestCase):
 
     def test_Capital_letter(self):
         create_text_log(self.capitalString)
-        s = check_file(self.capitalString)
+        s = check_file(unicode_conversion(self.capitalString))
         self.assertTrue(s,msg = "True")
 
 
@@ -114,17 +114,18 @@ class TESTlog(unittest.TestCase):
       global textPath
       path =textPath
       text = text_log()
+      print path
       textfile = text.make_report(path)
 
     
       def test_initial(self):
           textfile = self.text.make_report(self.path)
-          os.remove(self.path)
+          os.remove(unicode_conversion(self.path))
           self.text.initial(self.path) #manually checked
-          lines = [line.rstrip('').decode('cp949').encode('cp949') for line in open(self.path)]
-          check_lines = [str(date.today())+"\t\t\t에러 리포트\n","\n","\t\n",
-                       "사용한 텍스트 파일 주소:\t"+self.path+"\n","\n","로그 시작한 시간:\t\t "+str(datetime.now().replace(second=0, microsecond=0))+"\n","\n","\n",
-                       "\t\t" +"\t백업 과정 보고서\n","\n","\n"]
+          lines = [line.rstrip('') for line in open(unicode_conversion(self.path))]
+          check_lines = [unicode_conversion(str(date.today())+"\t\t\t에러 리포트\n"),"\n","\t\n",
+                       unicode_conversion("사용한 텍스트 파일 주소:\t")+self.path+"\n","\n",unicode_conversion("로그 시작한 시간:\t\t "+str(datetime.now().replace(second=0, microsecond=0))+"\n"),"\n","\n",
+                       "\t\t" +unicode_conversion("\t백업 과정 보고서\n"),"\n","\n"]
           print set(lines)-set(check_lines)
           self.assertEqual(lines,check_lines)
 
@@ -132,11 +133,11 @@ class TESTlog(unittest.TestCase):
       def test_success(self):
 
               text = text_log()
-              os.remove(self.path)
+              os.remove(unicode_conversion(self.path))
               text.success(self.path)
         
-              lines = [line.rstrip('\n').decode('cp949').encode('cp949') for line in open(self.path)]
-              check_lines = ["성공여부\t\t**\t #!!복사 성공!!#\t:)\n".rstrip('\n'),"코멘트\t\t\t\t"]
+              lines = [line.rstrip('\n') for line in open(unicode_conversion(self.path))]
+              check_lines = [unicode_conversion("성공여부\t\t**\t #!!복사 성공!!#\t:)\n".rstrip('\n')),unicode_conversion("코멘트\t\t\t\t")]
             
               self.assertEqual(lines,check_lines)
 
@@ -144,18 +145,18 @@ class TESTlog(unittest.TestCase):
       def test_failed(self):
            
               text = text_log()
-              os.remove(self.path)
+              os.remove(unicode_conversion(self.path))
               text.failed(self.path)
-              lines = [line.rstrip('\n').decode('cp949').encode('cp949') for line in open(self.path)]
-              check_lines = ["성공여부\t\t**\t #!!복사 실패!!#".rstrip('\n')+ "\tX(\n".rstrip('\n'),"코멘트\t\t\t\t"]
+              lines = [line.rstrip('\n') for line in open(unicode_conversion(self.path))]
+              check_lines = [unicode_conversion("성공여부\t\t**\t #!!복사 실패!!#".rstrip('\n')+ "\tX(\n".rstrip('\n')),unicode_conversion("코멘트\t\t\t\t")]
               self.assertEqual(lines,check_lines)
                                                         
       def test_writeError(self):
                text = text_log()
-               os.remove(self.path)
+               os.remove(unicode_conversion(self.path))
                text.error(self.path, "에러입니다?#@!(*&^$@!#%^&")
-               lines = [line for line in open(self.path)]
-               check_lines=["에러입니다?#@!(*&^$@!#%^&\n"]
+               lines = [line for line in open(unicode_conversion(self.path))]
+               check_lines=[unicode_conversion("에러입니다?#@!(*&^$@!#%^&\n")]
                self.assertEqual(lines,check_lines)
         
 class TESTfinal(unittest.TestCase):
@@ -163,11 +164,12 @@ class TESTfinal(unittest.TestCase):
     #가장 중요한 메인 함수 두개 검사. 
 
     pathDirectory = directoryPath
-    
-    if not os.path.isdir(pathDirectory):
-          os.mkdir(pathDirectory)
+    print pathDirectory
+    if not os.path.isdir((unicode(pathDirectory,"utf-8").encode('cp949'))):
+          os.mkdir(unicode(pathDirectory,"utf-8").encode('cp949'))
     N = 3000
     F = 7000
+    print pathDirectory
     text_log().make_report(pathDirectory+"\error.txt")
     path = pathDirectory+"\error.txt"
 
@@ -177,7 +179,7 @@ class TESTfinal(unittest.TestCase):
         for i in range(0, self.N):
              x = id_generator()
              file2 = text_log().make_report(self.pathDirectory+"\\"+x+".txt")
-             file_check1  = os.path.isfile(self.pathDirectory+"\\"+x+".txt")
+             file_check1  = os.path.isfile(unicode_conversion(self.pathDirectory+"\\"+x+".txt"))
              self.assertTrue(file_check1,msg = "True")
 
     
@@ -189,11 +191,12 @@ class TESTfinal(unittest.TestCase):
             text_log().make_report(self.pathDirectory+"\error.txt")
             text = text_log()
             text.error(self.path, x)
-            lines = [line.rstrip("\n") for line in open(self.path)]
+            lines = [line.rstrip("\n") for line in open(unicode_conversion(self.path))]
             check_lines=[x]
             self.assertEqual(lines,check_lines)  
 
-             
+def unicode_conversion(string):
+    return str(unicode(string,"utf-8").encode('cp949'))            
 if __name__ == '__main__': 
 
     unittest.main()

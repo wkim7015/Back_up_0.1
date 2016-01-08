e6e499b7963eba1ab6c81266179b8b2f3fda77b1
wkim7015 <u5125048@anu.edu.au>
2016-01-08 11:21:17 +0900

    Update TESTaddress_function.py

--- TESTUTF-8/TESTaddress_function.py
+++ TESTUTF-8/TESTaddress_function.py
@@ -24,6 +24,7 @@ from address_function import extract_address
 
 import unittest
 
+
 #########################===========================================================================================
 #TEST INPUTS
 
@@ -71,14 +72,38 @@ class TESTcopy_address(unittest.TestCase):
         self.assertEqual(test_copy,correctString)
         
 def unicode_conversion(string):
-    return str(unicode(string,"utf-8").encode('cp949'))  
+    return str(unicode(string,"utf-8").encode('cp949'))
+
+#################### Create a text file which has addresses as below.
+
+
+directoryName = os.path.dirname(__file__)
+if not os.path.exists(os.path.dirname(__file__) +"\\test_address.txt"):
+    
+    initialTest = open(os.path.dirname(__file__) +"\\test_address.txt",'a')
+    initialTest.write("C:\Users\Hojin\Desktop\exp02"+"\n")
+    initialTest.write("C:\Users\Hojin\Desktop\exp1"+"\n")
+    initialTest.write(unicode_conversion("C:\Users\Hojin\Desktop\ 새 폴더")+"\n")
+    initialTest.write("C:\Users\Hojin\Desktop\\remove_expire.pyc"+"\n")
+    initialTest.write("D:\Fakefiles.txt"+"\n")
+
+if not os.path.exists(os.path.dirname(__file__) +"\\test_null_address.txt"):
+    
+    initialTestNull = open(os.path.dirname(__file__) +"\\test_null_address.txt",'a')
+    initialTestNull.write(unicode_conversion("C:\Users\Hojin\Desktop\exp02")+"\n")
+    initialTestNull.write(unicode_conversion("C:\Users\Hojin\Desktop\exp1")+"\n")
+    initialTestNull.write( unicode_conversion("C:\Users\Hojin\Desktop\ 새 폴더")+"\n")
+    initialTestNull.write(unicode_conversion("C:\Users\Hojin\Desktop\\remove_expire.pyc")+"\n")
+    initialTestNull.write(unicode_conversion("D:\Fakefiles.txt"))
+                    
 ##################################################################==================================================================
 
 '''
 Below test requires a text file which contains a set of addresses as an array. Prior running the below function, it is neccessary to
 create the text file which contains the the addresses.When the textfiles are downloaded,below parameters do not require to change.
 '''
-textFile = r"C:\Users\Hojin\Desktop\test_address.txt"
+textFile = os.path.dirname(__file__) +"\\test_address.txt"
+#r"C:\Users\Hojin\Desktop\test_address.txt"
 
 "Then, please write the paths of the addresses below the array."
 
@@ -94,7 +119,9 @@ class TESTget_address(unittest.TestCase):
         global textFile
         TextFile = unicodes(textFile)
         global trueArray
-        test_link = open (TextFile, 'r' )
+        test_link = open (TextFile, 'r')
+#        print TextFile
+#        print test_link.readline()
         testArray = extract_address().get_address( test_link )
         #trueArray = [ "C:\Users\Hojin\Desktop\exp02","C:\Users\Hojin\Desktop\exp1",
         #             "C:\Users\Hojin\Desktop\ 새 폴더","C:\Users\Hojin\Desktop\\remove_expire.pyc", "D:\Fakefiles.txt" ]
@@ -102,8 +129,8 @@ class TESTget_address(unittest.TestCase):
         
     def test_null_address(self): #주소가 None 이나 빈공간 그리고 엔터키가 눌려진 공간과 섞여있을경우 주소를 가져오는지 검사
         
-        test_link2 = open (r"C:\Users\Hojin\Desktop\test_null_address.txt",'r')
-       
+    
+        test_link2 = open (directoryName +"\\test_null_address.txt",'r') 
         testArray = extract_address().get_address( test_link2 )
         
         trueArray = [unicode_conversion("C:\Users\Hojin\Desktop\exp02"),unicode_conversion("C:\Users\Hojin\Desktop\exp1"),
@@ -116,7 +143,7 @@ class TESTmain(unittest.TestCase):
 
     def test_main_01(self): #given
         
-        test_link2 = open ( r"C:\Users\Hojin\Desktop\test_null_address.txt",'r' )
+        test_link2 = open ( os.path.dirname(__file__) +"\\test_null_address.txt",'r' )
         #print test_link2
         testArray = extract_address().main( test_link2 )
         #print testArray
@@ -125,6 +152,8 @@ class TESTmain(unittest.TestCase):
         #print trueArray
         self.assertEqual( testArray, trueArray )
 
+
 if __name__ == '__main__': 
 
     unittest.main()
+

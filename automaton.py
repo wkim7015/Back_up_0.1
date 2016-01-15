4e754279a92a70c262951c45e0e43d2408060f23
wkim7015 <u5125048@anu.edu.au>
2016-01-15 11:23:24 +0900

    Update automaton.py

--- automaton.py
+++ automaton.py
@@ -3,9 +3,16 @@ import os
 sys.path.append(os.path.realpath('..'))
 from datetime import datetime
 from threading import Timer
-
+####-----------------------------------------
+''' This file is to automatically run the main function at the given time. The below parameters are required to be filled in
+'''
+Hour = 11
+Minute = 18
+Second = 0
+# Then the above will be excuted 11:18 am everyday.
+####------------------------------------------
 x=datetime.today()
-y=x.replace(day=x.day+1, hour=11, minute=18, second=0, microsecond=0)
+y=x.replace(day=x.day+1, hour=Hour, minute=Minute, second=Second, microsecond=0)
 delta_t=y-x
 
 secs=delta_t.seconds+1
